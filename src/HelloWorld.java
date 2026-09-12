a76a47ee68a078afe25d31e39e100cb62ead4194
Mohamed Arsath <147827230+arsath134@users.noreply.github.com>
2026-09-12 16:08:36 +0530

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,1 +1,7 @@
+public class HelloWorld {
 
+    public static void main(String[] args) {
+        System.out.println("Hello from Jenkins!");
+        System.out.println("Java application is running successfully.");
+    }
+}
